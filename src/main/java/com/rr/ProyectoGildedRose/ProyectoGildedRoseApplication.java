b545ca23cd9be57e891a7eb2de96d0a2c2a7b6ee
Renzo_Romeri <renzo.romeri@gmail.com>
2025-02-07 15:34:09 +0100

    Se limpia codigo

--- src/main/java/com/rr/ProyectoGildedRose/ProyectoGildedRoseApplication.java
+++ src/main/java/com/rr/ProyectoGildedRose/ProyectoGildedRoseApplication.java
@@ -22,20 +22,13 @@ public class ProyectoGildedRoseApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-//        List<Item> items = List.of(
-//                new Item("Aged Brie", 10, 20),
-//                new Item("Backstage passes", 15, 30),
-//                new Item("Sulfuras", 5, 80),
-//                new Item("Conjured", 3, 6),
-//                new Item("Default", 9, 7)
-//        );
-        
+
         List<Item> items = List.of(
-            new ItemBuilder().withName("Aged Brie").withSellIn(10).withQuality(20).build(),
-            new ItemBuilder().withName("Backstage passes").withSellIn(15).withQuality(30).build(),
-            new ItemBuilder().withName("Sulfuras").withSellIn(5).withQuality(80).build(),
-            new ItemBuilder().withName("Conjured").withSellIn(3).withQuality(6).build(),
-            new ItemBuilder().withName("Default").withSellIn(9).withQuality(7).build()
+                new ItemBuilder().withName("Aged Brie").withSellIn(10).withQuality(20).build(),
+                new ItemBuilder().withName("Backstage passes").withSellIn(15).withQuality(30).build(),
+                new ItemBuilder().withName("Sulfuras").withSellIn(5).withQuality(80).build(),
+                new ItemBuilder().withName("Conjured").withSellIn(3).withQuality(6).build(),
+                new ItemBuilder().withName("Default").withSellIn(9).withQuality(7).build()
         );
 
         items.forEach(itemService::addItem);
